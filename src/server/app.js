1409cc88f99d45879f20165598ffc18cfe49db30
ghali belmkaddame <ghalibelmne00@gmail.com>
2024-11-18 17:43:54 +0100

    removed bootstrap lol

--- src/server/app.js
+++ src/server/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const routes = require('./routes/routes');
+require('dotenv').config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')))
+
+app.use(routes);
 
 app.listen(port, "0.0.0.0", () => {
     console.log(`server is running on ${port}`)
