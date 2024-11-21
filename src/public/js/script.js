307a39f5397b27bad906e766b9aa636ea5b0b648
ghali belmkaddame <ghalibelmne00@gmail.com>
2024-11-22 00:53:35 +0100

    v0.1

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -31,3 +31,21 @@ fetch('/api/projects')
     });
   })
   .catch(error => console.error('Error fetching projects:', error));
+
+const scrollLinks = document.querySelectorAll('a[href^="#"]');
+
+scrollLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - document.querySelector('header').offsetHeight,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
